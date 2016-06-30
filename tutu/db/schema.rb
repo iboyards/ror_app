7824fe06f78259e8116a59e224b9d754a75ba5fc
iboyards <iboyards@gmail.com>
2016-06-30 14:21:13 +0300

    Add user model

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629081512) do
+ActiveRecord::Schema.define(version: 20160629082334) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20160629081512) do
     t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
