c379b95aac3e94cef1d93927d577e399c69a2fa3
iboyards <iboyards@gmail.com>
2016-07-08 13:39:24 +0300

    Edit validates car

--- tutu/app/models/car.rb
+++ tutu/app/models/car.rb
@@ -1,9 +1,7 @@
 class Car < ActiveRecord::Base
   
   belongs_to :train
-  
-  validates :car_type, presence: true
-  validates :up_places, presence: true
-  validates :low_places, presence: true
-  
+
+  validates :car_type, :up_places, :low_places, presence: true
+    
 end
