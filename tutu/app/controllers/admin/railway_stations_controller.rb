6a1c9e9d5d8cfea505a61959a3ab24bd3ce3fbc9
iboyards <iboyards@gmail.com>
2016-08-01 22:44:18 +0300

    Исправил редирект на admin route

--- tutu/app/controllers/admin/railway_stations_controller.rb
+++ tutu/app/controllers/admin/railway_stations_controller.rb
@@ -56,17 +56,17 @@ class Admin::RailwayStationsController < ApplicationController
 
   def set_number     
     @railway_station.update_number(@route, params[:number])
-    redirect_to @route
+    redirect_to [:admin, @route]
   end
 
   def update_arrival_time    
     @railway_station.update_arrival_time(@route, params[:arrival_time])
-    redirect_to @route
+    redirect_to [:admin, @route]
   end
 
   def update_departure_time    
     @railway_station.update_departure_time(@route, params[:departure_time])
-    redirect_to @route
+    redirect_to [:admin, @route]
 end
 
   # DELETE /railway_stations/1
